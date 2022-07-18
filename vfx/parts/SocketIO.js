2e92909683b955442a59f958d401868deb5e12eb
Wong Lok <wonglok@users.noreply.github.com>
2022-07-18 09:02:14 +0800

    yo

--- vfx/parts/SocketIO.js
+++ vfx/parts/SocketIO.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+
 export function SocketIO() {
+  useEffect(() => {
+    let setup = async () => {
+      const { io } = await import("socket.io-client");
+      const SERVER_URL = "http://localhost:1337";
+      const socket = io(SERVER_URL);
+
+      //  wait until socket connects before adding event listeners
+      socket.on("connect", () => {
+        socket.on("message:update", (data) => {
+          console.log(data);
+        });
+      });
+    };
+
+    setup();
+  }, []);
   return (
     <div>
-      123
+      SocketIO
       {/*  */}
     </div>
   );
